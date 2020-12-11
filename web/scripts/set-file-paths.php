41926e9d716fbd3e2ddd8444274d1e9657a9f6ed
MaxTMcCoy <maxmccoy@creighton.edu>
2020-12-11 10:22:33 -0600

    correct vset key for temp directory

--- web/scripts/set-file-paths.php
+++ web/scripts/set-file-paths.php
@@ -5,6 +5,6 @@ passthru("drush vset file_public_path sites/default/files");
 echo "set private\n";
 passthru("drush vset file_private_path sites/default/files/private");
 echo "set temp\n";
-passthru("drush vset file_temp_path sites/default/files/private/temp");
+passthru("drush vset file_temporary_path sites/default/files/private/temp");
 echo "rebuild cache\n";
 passthru("drush cr");

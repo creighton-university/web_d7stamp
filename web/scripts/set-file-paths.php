ed3dc233c1cc9a8d8709ec840aa2f38f6330f42b
MaxTMcCoy <maxmccoy@creighton.edu>
2020-12-11 10:17:46 -0600

    fix file path copy pasta

--- web/scripts/set-file-paths.php
+++ web/scripts/set-file-paths.php
@@ -3,8 +3,8 @@
 echo "set public\n";
 passthru("drush vset file_public_path sites/default/files");
 echo "set private\n";
-passthru("drush vset file_public_path sites/default/files/private");
+passthru("drush vset file_private_path sites/default/files/private");
 echo "set temp\n";
-passthru("drush vset file_public_path sites/default/files/private/temp");
+passthru("drush vset file_temp_path sites/default/files/private/temp");
 echo "rebuild cache\n";
 passthru("drush cr");

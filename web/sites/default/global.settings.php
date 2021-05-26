c4fd186d765695254193d8e7ed5fc01b69263152
MaxTMcCoy <maxmccoy@creighton.edu>
2021-05-26 13:54:58 -0500

    user

--- web/sites/default/global.settings.php
+++ web/sites/default/global.settings.php
@@ -7,7 +7,7 @@
 $databases['default']['default'] = array(
   'driver' => 'mysql',
   'database' => $_ENV['DB_NAME'],
-  'username' => $_ENV['DB_NAME'],
+  'username' => $_ENV['DB_USER'],
   'password' => $_ENV['DB_PASSWORD'],
   'host' => $_ENV['DB_HOST'],
   'port' => $_ENV['DB_PORT']

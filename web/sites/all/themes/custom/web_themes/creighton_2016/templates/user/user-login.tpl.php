876df3c7479efe7f6096c3dff9b38b203d399bc2
MaxTMcCoy <maxmccoy@creighton.edu>
2021-05-26 11:08:12 -0500

    remove hard coded shib block

--- web/sites/all/themes/custom/web_themes/creighton_2016/templates/user/user-login.tpl.php
+++ web/sites/all/themes/custom/web_themes/creighton_2016/templates/user/user-login.tpl.php
@@ -19,17 +19,7 @@
       <li><a href="#tabs-1">User Login</a></li>
       <li><a href="#tabs-2">Administrator Login</a></li>
     </ul>
-    <div id="tabs-1">
-      <p>To login, use the 'Login to Blue' link below with your NetID and password:</p>
-      <?php
-        $shib_auth = block_load('shib_auth', 'login_box');
-        $shib_auth_output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($shib_auth))));
-        print $shib_auth_output;
-      ?>
-      <p>Creighton University uses single sign-on. The goal is to provide an easy to use and secure authentication environment for users.<br />
-      <a href="http://doit.creighton.edu/network-security/security/web-single-sign" target="_blank">Learn more</a>
-      </p>
-    </div>
+
     <div id="tabs-2">
       <?php
       // split the username and password from the submit button so we can put in links above

667a2461f398ac1b043b057f85fffcd941a84300
MaxTMcCoy <maxmccoy@creighton.edu>
2021-05-26 13:40:52 -0500

    new db creds for hub

--- web/sites/default/global.settings.php
+++ web/sites/default/global.settings.php
@@ -4,24 +4,24 @@
  * @file
  * D7 Creighton site-specific configuration file.
  */
-
 $databases['default']['default'] = array(
   'driver' => 'mysql',
   'database' => $_ENV['DB_NAME'],
-  'username' => $_ENV['DB_USER'],
+  'username' => $_ENV['DB_NAME'],
   'password' => $_ENV['DB_PASSWORD'],
   'host' => $_ENV['DB_HOST'],
   'port' => $_ENV['DB_PORT']
 );
+
 
 $databases['hub']['default'] = array(
   'server' => 'live-legacy-creighton-hub.pantheonsite.io',
   'driver' => 'mysql',
   'database' => 'pantheon',
   'username' => 'pantheon',
-  'password' => '43084747a27743219e0dce5b7d24174f',
-  'host' => 'dbserver.live.8f16d3f9-7730-4512-a10c-11663041c508.drush.in',
-  'port' => '10152',
+  'password' => '11735cbf939d48a0b273a21b089802a3',
+  'host' => 'ddbserver.live.8f16d3f9-7730-4512-a10c-11663041c508.drush.in',
+  'port' => '13065',
   'prefix' => '',
 );
 

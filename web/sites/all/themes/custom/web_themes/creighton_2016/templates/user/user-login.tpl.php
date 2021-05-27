80091e87ec2ddf59e587e3c5c78492c43398a3a2
MaxTMcCoy <maxmccoy@creighton.edu>
2021-05-27 16:28:08 -0500

    remove tabs

--- web/sites/all/themes/custom/web_themes/creighton_2016/templates/user/user-login.tpl.php
+++ web/sites/all/themes/custom/web_themes/creighton_2016/templates/user/user-login.tpl.php
@@ -1,39 +1,35 @@
 <?php
 
-  //drupal_add_library('system', 'ui.tabs');
-  drupal_add_js('misc/ui/jquery.ui.core.min.js', array('group' => 'JS_LIBRARY'));
-  drupal_add_js('misc/ui/jquery.ui.widget.min.js');
-  drupal_add_js('misc/ui/jquery.ui.tabs.min.js');
-  drupal_add_js('jQuery(document).ready(function(){jQuery("#tabs").tabs();});', 'inline');
-  drupal_add_css('misc/ui/jquery.ui.tabs.css');
-  drupal_add_css('#admin-links {display:none !important;} #tabs .block-title {display: none !important}', 'inline');
+//drupal_add_library('system', 'ui.tabs');
+drupal_add_js('misc/ui/jquery.ui.core.min.js', array('group' => 'JS_LIBRARY'));
+drupal_add_js('misc/ui/jquery.ui.widget.min.js');
+drupal_add_js('misc/ui/jquery.ui.tabs.min.js');
+drupal_add_js('jQuery(document).ready(function(){jQuery("#tabs").tabs();});', 'inline');
+drupal_add_css('misc/ui/jquery.ui.tabs.css');
+drupal_add_css('#admin-links {display:none !important;} #tabs .block-title {display: none !important}', 'inline');
 ?>
 
 <div class="creighton-user-login-form-wrapper">
 
-<div class="login-wrapper">
+  <div class="login-wrapper">
 
-    <h2><?php print render($intro_text); ?></h2>
+    <h2>Administrator Login</h2>
     <div id="tabs" class="tabs clearfix">
-    <ul class="tabs primary clearfix">
-      <li><a href="#tabs-2">Administrator Login</a></li>
-    </ul>
-    <div id="tabs-2">
-      <?php
-      // split the username and password from the submit button so we can put in links above
-      print drupal_render($form['name']);
-      print drupal_render($form['pass']);
-      ?>
+      <div id="tabs-1">
+        <?php
+        // split the username and password from the submit button so we can put in links above
+        print drupal_render($form['name']);
+        print drupal_render($form['pass']);
+        ?>
 
-      <?php
-      print drupal_render($form['form_build_id']);
-      print drupal_render($form['form_id']);
-      print drupal_render($form['actions']);
-      ?>
+        <?php
+        print drupal_render($form['form_build_id']);
+        print drupal_render($form['form_id']);
+        print drupal_render($form['actions']);
+        ?>
+      </div>
     </div>
-    </div>
-
-</div><!--//login-wrapper-->
-
-</div><!--//creighton-user-login-form-wrapper -->
-
+  </div>
+  <!--//login-wrapper-->
+</div>
+<!--//creighton-user-login-form-wrapper -->

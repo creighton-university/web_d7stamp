510cef2cc262ee3ff1e766c864b3f2995d69e535
McCoy <maxmccoy@creighton.edu>
2021-06-10 16:17:12 -0500

    test on dev envs, add some protection to global settings file

--- web/sites/default/global.settings.php
+++ web/sites/default/global.settings.php
@@ -25,25 +25,31 @@ $databases['hub']['default'] = array(
   'username' => 'pantheon',
   'password' => '235f844fd9ce474a82fd5d37d47845e5',
   'host' => 'dbserver.live.8f16d3f9-7730-4512-a10c-11663041c508.drush.in',
-  'port' => '12345',
+  'port' => '11597',
   'prefix' => '',
 );
 
+//bandaid fix for the hub credentials changing periodically on pantheon :)
 if(file_exists('sites/default/files/private/secrets.json')){
+  //this file is updated by a periodic workflow on web_d7stamp github actions
+  //the workflow is called update_hub_creds (found in .github/workflows/update_hub_creds.yml)
   $json_text = file_get_contents('sites/default/files/private/secrets.json');
   $secrets = json_decode($json_text, TRUE);
-  $hub_db_creds = parse_url($secrets['hub_db_url']);
+  if(isset($secrets['hub_db_url'])){
+    //the credentials for hub are stored as a url
+    $hub_db_creds = parse_url($secrets['hub_db_url']);
 
-  $databases['hub']['default'] = array(
-    'server' => 'live-legacy-creighton-hub.pantheonsite.io',
-    'driver' => 'mysql',
-    'database' => 'pantheon',
-    'username' => $hub_db_creds['user'],
-    'password' => $hub_db_creds['pass'],
-    'host' => $hub_db_creds['host'],
-    'port' => $hub_db_creds['port'],
-    'prefix' => '',
-  );
+    $databases['hub']['default'] = array(
+      'server' => 'live-legacy-creighton-hub.pantheonsite.io',
+      'driver' => 'mysql',
+      'database' => 'pantheon',
+      'username' => $hub_db_creds['user'],
+      'password' => $hub_db_creds['pass'],
+      'host' => $hub_db_creds['host'],
+      'port' => $hub_db_creds['port'],
+      'prefix' => '',
+    );
+  }
 }
 
 

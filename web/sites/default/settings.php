6594d2b5c1f92c610d020504c33f159b3f6e5f86
Joshua Minarick <joshuaminarick@creighton.edu>
2021-06-16 16:05:18 -0500

    Updated settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -670,16 +670,32 @@ if (file_exists($global_settings)) {
 
 /**
  * @file
- * 
- * @var Array $varyHeaders
- * 
- * @var Array $environments
- * 
+ * AGCDN domain masking settings.
  */
+
+/**
+ * @var Array $varyHeaders
+ * Headers that will be accessed for domain masking.
+ */
+$varyHeaders = [
+  'x-masked-host',
+  'x-masked-path',
+];
+
+/**
+ * @var Array $environments
+ * Enviroments to monitor for AGCDN connections.
+ */
+$environments = array(
+  'dev',
+  'test',
+  'live',
+);
 
 $varyString = implode(', ', $varyHeaders);
 header("Vary: ${varyString}", FALSE);
-if (in_array($_ENV['PANTHEON_ENVIRONMENT'], $environments) && (isset($_SERVER['HTTP_X_MASKED_HOST']))) { 
+
+if (in_array($_ENV['PANTHEON_ENVIRONMENT'], $environments) && (isset($_SERVER['HTTP_X_MASKED_HOST']))) {
   $base_url = "https://" . $_SERVER['HTTP_X_MASKED_HOST'];
   if (isset($_SERVER['HTTP_X_MASKED_PATH'])) {
     $base_url = $base_url . $_SERVER['HTTP_MASKED_PATH'];
